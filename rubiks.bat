8368a17987143da1214ab952c59b58355295827e
meridianlabs <meridianlabsin@gmail.com>
2026-07-10 17:00:38 +0530

    Add files via upload

--- rubiks.bat
+++ rubiks.bat
@@ -1,4 +1,10 @@
 @echo off
 cd /d "%~dp0"
+echo =======================================
+echo Checking and installing requirements...
+echo =======================================
+pip install -r requirements.txt
+playwright install
+cls
 python main.py
 pause
